bfcf156e4235756d38f3813e039748eb002294fb
RecherDan <recher.dan@gmail.com>
2016-08-14 20:12:23 +0300

    close

--- scripts/doortester.js
+++ scripts/doortester.js
@@ -4,7 +4,7 @@ var net = require('net');
 var client = new net.Socket();
 client.connect(1337, '127.0.0.1', function() {
 	console.log('Connected');
-	client.write('open');
+	client.write('close');
 });
 
 client.on('data', function(data) {
