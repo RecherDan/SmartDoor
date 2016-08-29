f0c2d0a149d48caea69a9866548127deba6f0fff
RecherDan <recher.dan@gmail.com>
2016-08-29 21:36:42 +0300

    sdfsdf

--- scripts/DoorStatusAnalog.js
+++ scripts/DoorStatusAnalog.js
@@ -1,4 +1,6 @@
 var mraa = require('mraa'); //require mraa
+var sleep = require('sleep'); //require mraa
+
 var analogPin0 = new mraa.Aio(0); //to indecat if the door isclose or not useing a potensiometer
 var analogValue = analogPin0.read(); //read the value of the analog pin
 var maxStepsToOpen = 7000; // how many maximum!!!!! steps to open or close the door the motore sould do
