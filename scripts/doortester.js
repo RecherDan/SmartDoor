2830afa3fe3628ecbb0f96c909c1953db4a0630c
RecherDan <recher.dan@gmail.com>
2016-08-29 22:09:53 +0300

    sdfsdf

--- scripts/doortester.js
+++ scripts/doortester.js
@@ -1,10 +1,11 @@
 
 var net = require('net');
+var mode = process.argv[2];
 
 var client = new net.Socket();
 client.connect(1337, '127.0.0.1', function() {
 	console.log('Connected');
-	client.write('close');
+	client.write(mode);
 });
 
 client.on('data', function(data) {
