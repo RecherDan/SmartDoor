770901274370dbf8fb60eaabd00c14dbe23382cc
RecherDan <recher.dan@gmail.com>
2016-08-29 21:46:04 +0300

    sdfsdf

--- scripts/DoorStatusAnalog.js
+++ scripts/DoorStatusAnalog.js
@@ -1,13 +1,18 @@
 var mraa = require('mraa'); //require mraa
-var sleep = require('sleep'); //require mraa
 
 var analogPin0 = new mraa.Aio(0); //to indecat if the door isclose or not useing a potensiometer
 var analogValue = analogPin0.read(); //read the value of the analog pin
 var maxStepsToOpen = 7000; // how many maximum!!!!! steps to open or close the door the motore sould do
-var half_time_Of_Sleep_Between_Steps=1200;
 
-for(var i = 0; i < maxStepsToOpen ;i++){
-    chek=analogPin0.read();
-     console.log("analog data from A0: %d",chek);
-     sleep.usleep(2*half_time_Of_Sleep_Between_Steps) ;
- }
+
+
+var net = require('net');
+
+var server = net.createServer(function(socket) {
+	socket.setKeepAlive(true,60000);
+	socket.on('data', function(data) {
+			socket.write(analogPin0.read());
+	});
+});
+
+server.listen(6001, '127.0.0.1');
