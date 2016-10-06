0634d47a9944dd65b6befc0125f2e4dc7884e8db
DanRecher <recher.dan@gmail.com>
2016-10-06 19:40:14 +0300

    ok

---
+++ scripts/online.js
@@ -0,0 +1,42 @@
+var Firebase = require("firebase");
+var getIP = require('external-ip')();
+var minutes = 0.01, the_interval = minutes * 60 * 1000;
+var config = {
+	    apiKey: "AIzaSyCRpzldmrnwtOf7M_TBBNGFofyswZ2IifQ",
+	    authDomain: "smartdoor-2f29b.firebaseapp.com",
+	    databaseURL: "https://smartdoor-2f29b.firebaseio.com",
+	    storageBucket: "",
+	    messagingSenderId: "693048105512"
+	  };
+Firebase.initializeApp(config);
+var database = Firebase.database();
+var doorstatus = "closed";
+var smokedet = "clean";
+var lastevent = "13:20";
+var alarm = "off";
+var eip = "";
+
+console.log("start updating online status");
+getIP(function (err, ip) {
+    if (err) {
+        // every service in the list has failed 
+        throw err;
+    }
+    eip = ip;
+});
+
+setInterval(function() {
+  console.log("I am doing my 0.1 minutes check");
+  var d = new Date();
+  var rootref = database.ref().child('doors');
+  var doorref = rootref.child('dan-door');
+  //exec tester.js to receive door information
+  doorref.set({ip: eip,
+	  time: d.getTime(),
+	  doorstatus: doorstatus,
+	  smokedetect: smokedet,
+	  alarm: alarm,
+	  lastevent: lastevent
+	  });
+  // do your stuff here
+}, the_interval);
