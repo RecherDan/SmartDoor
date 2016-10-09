1951527ac457bdf2d2a77475aac1f8bfdda3898a
DanRecher <recher.dan@gmail.com>
2016-10-09 18:29:54 +0300

    ggg

--- scripts/online.js
+++ scripts/online.js
@@ -29,6 +29,40 @@ getIP(function (err, ip) {
 
 database.ref().child('doors').child(doorconfig.doorname).child('todo').on("value", function(snapshot) {
 	  console.log("todo " + snapshot.val());
+	  if (snapshot.val() != "null" ) {
+		  var valid = false;
+		  var port = 6001;
+		  var mode = "";
+		  if ( snapshot.val()  == "Lock" ) {
+			  valid = true;
+			  port = 6001;
+			  mode = Close;
+		  } 
+		  if ( snapshot.val()  == "Unlock" ) {
+			  valid = true;
+			  port = 6001;
+			  mode = Open;
+		  } 
+		  if ( valid ) {
+			  var client = new net.Socket();
+			  client.connect(port, '127.0.0.1', function() {
+			  	console.log('Connected');
+			  	client.write(mode);
+			  });
+
+			  client.on('data', function(data) {
+			  	console.log('Received: ' + data);
+			  	client.destroy(); // kill client after server's response
+			  });
+
+			  client.on('close', function() {
+			  	console.log('Connection closed');
+			  });
+		  }
+	  }
+		  
+	  database.ref().child('doors').child(doorconfig.doorname).child('todo').set("null");
+	  
 	});
 
 setInterval(function() {
