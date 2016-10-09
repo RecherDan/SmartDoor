7700c9e1ef02a1f31ef8ec59abcdd97ea1212287
DanRecher <recher.dan@gmail.com>
2016-10-09 21:42:16 +0300

    dsf

--- scripts/MotionDetect.js
+++ scripts/MotionDetect.js
@@ -1,15 +1,12 @@
 var mraa = require('mraa'); //require mraa
 var minutes = 0.000001, the_interval = minutes * 60 * 1000;
-var analogPin1 = new mraa.Aio(2); //to indecat if the door isclose or not useing a potensiometer
+var myDigitalPin6 = new mraa.Gpio(6);
+myDigitalPin6.dir(mraa.DIR_IN);
 var childProcess = require('child_process'), child;
 var lastread = 1000;
 var margin = 10;
 setInterval(function() {
-	if (((analogPin1.read() - lastread) > margin ) || ((analogPin1.read() - lastread) < -margin )) {
-		console.log(analogPin1.read());
-	}
-	
-	lastread = analogPin1.read();
+	console.log(myDigitalPin6.read());
 	
 	
 }, the_interval);
