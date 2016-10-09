4037197c95a9dac9b5372a35e00a58ba504a595b
DanRecher <recher.dan@gmail.com>
2016-10-09 21:24:18 +0300

    sdfsdf

--- scripts/Knockdetect.js
+++ scripts/Knockdetect.js
@@ -1,7 +1,7 @@
 var mraa = require('mraa'); //require mraa
 var minutes = 0.000001, the_interval = minutes * 60 * 1000;
 var analogPin1 = new mraa.Aio(3); //to indecat if the door isclose or not useing a potensiometer
-
+var childProcess = require('child_process'), child;
 var KnockCount = 0;
 var lastKnock = 0;
 var minThreshold = 100;
@@ -20,5 +20,14 @@ setInterval(function() {
 	if ( KnockCount >= 3 ) {
 		KnockCount = 0;
 		console.log("Took Took Took");
+		child = childProcess.exec('node scripts/sendnotification.js "Took took" "someonw knoked your door"', function (error, stdout, stderr) {
+			   if (error) {
+			     console.log(error.stack);
+			     console.log('Error code: '+error.code);
+			     console.log('Signal received: '+error.signal);
+			   }
+			   console.log('Child Process STDOUT: '+stdout);
+			   console.log('Child Process STDERR: '+stderr);
+			 });
 	}	
 }, the_interval);
