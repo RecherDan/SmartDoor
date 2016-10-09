7c3bebd36c574064cca3cb83e9076622f425e8a8
DanRecher <recher.dan@gmail.com>
2016-10-09 21:35:48 +0300

    sdfsdf

--- scripts/MotionDetect.js
+++ scripts/MotionDetect.js
@@ -3,7 +3,7 @@ var minutes = 0.000001, the_interval = minutes * 60 * 1000;
 var analogPin1 = new mraa.Aio(2); //to indecat if the door isclose or not useing a potensiometer
 var childProcess = require('child_process'), child;
 var lastread = 1000;
-var margin = 100;
+var margin = 10;
 setInterval(function() {
 	if (((analogPin1.read() - lastread) > margin ) || ((analogPin1.read() - lastread) < -margin )) {
 		console.log(analogPin1.read());
