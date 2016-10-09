5ceebce58a46b962507e8b8b6f569dc8455bdc14
DanRecher <recher.dan@gmail.com>
2016-10-09 21:58:46 +0300

    dsfsdf

--- scripts/MotionDetect.js
+++ scripts/MotionDetect.js
@@ -2,16 +2,28 @@ var mraa = require('mraa'); //require mraa
 var minutes = 0.000001, the_interval = minutes * 60 * 1000;
 var myDigitalPin6 = new mraa.Gpio(6);
 myDigitalPin6.dir(mraa.DIR_IN);
-var childProcess = require('child_process'), child;
-var lastread = 1000;
-var margin = 10;
+var doorconfig = require('./config'); // door configuration
+var Firebase = require("firebase");
+var config = {
+	    apiKey: "AIzaSyCRpzldmrnwtOf7M_TBBNGFofyswZ2IifQ",
+	    authDomain: "smartdoor-2f29b.firebaseapp.com",
+	    databaseURL: "https://smartdoor-2f29b.firebaseio.com",
+	    storageBucket: "",
+	    messagingSenderId: "693048105512"
+	  };
+Firebase.initializeApp(config);
+var database = Firebase.database();
+var rootref = database.ref().child('doors');
+var doorref = rootref.child(doorconfig.doorname);
 setInterval(function() {
 	if (myDigitalPin6.read() == 1 ) {
 		console.log("Movement!")
+		doorref.child('move').set('true');
 		    var stop = new Date().getTime();
 			while(new Date().getTime() < stop + 5000) {
 				;
 			}
+		doorref.child('move').set('false');
 	}
 
 	
