30895ac6000954c180f584deb443206e91074308
DanRecher <recher.dan@gmail.com>
2016-10-09 21:02:56 +0300

    sdfsdf

--- scripts/sendnotification.js
+++ scripts/sendnotification.js
@@ -1,5 +1,7 @@
-var FCM = require('fcm-push');
+var FCM = require('fcm-node');
 var Firebase = require("firebase");
+var doorconfig = require('./config'); // door configuration
+
 var config = {
 	    apiKey: "AIzaSyCRpzldmrnwtOf7M_TBBNGFofyswZ2IifQ",
 	    authDomain: "smartdoor-2f29b.firebaseapp.com",
@@ -13,8 +15,9 @@ var database = Firebase.database();
 var rootref = database.ref().child('users');
 //var doorref = rootref.child(doorconfig.doorname);
 
-var serverKey = 'https://smartdoor-2f29b.firebaseio.com';
-//var fcm = new FCM(serverKey);
+var serverKey = '';
+var fcm = new FCM(serverKey);
+
 
 
 var query = Firebase.database().ref("users").orderByKey();
@@ -24,8 +27,28 @@ snapshot.forEach(function(childSnapshot) {
   var key = childSnapshot.key;
   // childData will be the actual contents of the child
   var childData = childSnapshot.val();
-  console.log(key);
-  console.log(childSnapshot.child("toekn").val());
-  console.log(childSnapshot.child("door").val());
+  if ( doorconfig.doorname == childSnapshot.child("door").val()) {
+	  console.log(key);
+	  console.log(childSnapshot.child("toekn").val());
+	  console.log(childSnapshot.child("door").val());  
+	  
+	  var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+			    to: childSnapshot.child("toekn").val(), 
+			    
+			    notification: {
+			        title: 'Title of your push notification', 
+			        body: 'Body of your push notification' 
+			    }
+			};
+
+			fcm.send(message, function(err, response){
+			    if (err) {
+			        console.log("Something has gone wrong!");
+			    } else {
+			        console.log("Successfully sent with response: ", response);
+			    }
+			});
+  }
+
 });
 });
