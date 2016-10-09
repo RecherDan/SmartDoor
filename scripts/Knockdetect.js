bc6091ef4dae88bf488e1df0621f18d44cc8255b
DanRecher <recher.dan@gmail.com>
2016-10-09 19:48:23 +0300

    fcgdfg

--- scripts/Knockdetect.js
+++ scripts/Knockdetect.js
@@ -1,5 +1,5 @@
 var mraa = require('mraa'); //require mraa
-var minutes = 0.01, the_interval = minutes * 60 * 1000;
+var minutes = 0.000001, the_interval = minutes * 60 * 1000;
 var analogPin1 = new mraa.Aio(3); //to indecat if the door isclose or not useing a potensiometer
 
 setInterval(function() {
