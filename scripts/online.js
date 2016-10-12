6f7e1aaf67b5bb03b6c81c699283631e264519de
DanRecher <recher.dan@gmail.com>
2016-10-12 18:49:03 +0300

    sdfsdf

--- scripts/online.js
+++ scripts/online.js
@@ -30,7 +30,7 @@ getIP(function (err, ip) {
 });
 
 database.ref().child('doors').child(doorconfig.doorname).on("value", function(snapshot) {
-		console.log(snapshot.child('log'));
+		console.log(snapshot.child('test'));
 	  if (snapshot.child('todo').val() != "null" ) {
 		  database.ref().child('doors').child(doorconfig.doorname).child('todo').set("null");
 		  console.log("todo " + snapshot.child('todo').val());
