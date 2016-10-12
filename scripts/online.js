7dd96e4227cc009014080621fb05dad2c5b55429
DanRecher <recher.dan@gmail.com>
2016-10-12 18:51:25 +0300

    sdfsdf

--- scripts/online.js
+++ scripts/online.js
@@ -32,7 +32,7 @@ getIP(function (err, ip) {
 database.ref().child('doors').child(doorconfig.doorname).on("value", function(snapshot) {
 		
 	  if (snapshot.child('todo').val() != "null" ) {
-		  console.log(snapshot.child('test'));
+		  console.log(snapshot.child('test').val());
 		  database.ref().child('doors').child(doorconfig.doorname).child('todo').set("null");
 		  console.log("todo " + snapshot.child('todo').val());
 		  var valid = false;
