207bf984648cbf1d38e70b97556b9948b857fe7d
DanRecher <recher.dan@gmail.com>
2016-10-14 11:49:21 +0300

    sdgsdfg

---
+++ scripts/btkeepalive.js
@@ -0,0 +1,18 @@
+var minutes = 0.5, the_interval = minutes * 60 * 1000;
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/rfcomm0');
+	port.on('open', function() {
+			onsole.log("connected");
+	});
+	 
+	// open errors will be emitted as an error event 
+	port.on('error', function(err) {
+	  console.log('Error: ', err.message);
+	})	
+	port.on('data', function(data) {
+		console.log('bt recived:' + data);
+	});
+
+setInterval(function() {
+	port.write("1");
+}, the_interval);
