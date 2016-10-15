9fbf0023a6d48b87fb02826fbd7966318a83566d
DanRecher <recher.dan@gmail.com>
2016-10-15 22:01:47 +0300

    dsgdfg

--- scripts/getlanip.sh
+++ scripts/getlanip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/sbin/ifconfig | /usr/bin/awk '/wlan0/{getline; print}' | /usr/bin/cut -d\":\" -f2 | /usr/bin/cut -d\" \" -f1
+echo `/sbin/ifconfig | /usr/bin/awk '/wlan0/{getline; print}' | /usr/bin/cut -d\":\" -f2 | /usr/bin/cut -d\" \" -f1`
