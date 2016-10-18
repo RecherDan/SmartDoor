d7d31736d6e3accf9b41e7d5811220452f0ea6b1
DanRecher <recher.dan@gmail.com>
2016-10-18 15:16:38 +0300

    sdfsdf

--- scripts/SmokeDetect.js
+++ scripts/SmokeDetect.js
@@ -6,17 +6,41 @@ var analogPin1 = new mraa.Aio(1); //to indecat if the door isclose or not useing
 var analogValue = analogPin1.read(); //read the value of the analog pin
 
 
+var minutes = 0.1, the_interval = minutes * 60 * 1000;
+
+var doorconfig = require('./config'); // door configuration
+var Firebase = require("firebase");
+var config = {
+	    apiKey: "AIzaSyCRpzldmrnwtOf7M_TBBNGFofyswZ2IifQ",
+	    authDomain: "smartdoor-2f29b.firebaseapp.com",
+	    databaseURL: "https://smartdoor-2f29b.firebaseio.com",
+	    storageBucket: "",
+	    messagingSenderId: "693048105512"
+	  };
+
 function SmokeDetect() {
 	//TODO: implement to return 1 if smoke detected or 0 else.
-	
-	return analogPin1.read().toString()
+	if ( analogPin1.read() > doorconfig.SmokeThresh) 
+		return 1;
+	return 0
 }
 
-var server = net.createServer(function(socket) {
-	socket.setKeepAlive(true,60000);
-	socket.on('data', function(data) {
-			socket.write(SmokeDetect());
-	});
-});
+Firebase.initializeApp(config);
+var database = Firebase.database();
+var rootref = database.ref().child('doors');
+var doorref = rootref.child(doorconfig.doorname);
+var Smokecount = 0;
+var SmokeTime = 0;
 
-server.listen(6004, '127.0.0.1');
+setInterval(function() {
+	if ( SmokeDetect() == 1 ) {
+		Smokecount++;
+		if ( Smokecount >= 3 )
+			doorref.child('smokedetect').set('Detected');
+	}
+	if ( SmokeDetect() == 0 ) {
+		Smokecount = 0;
+		doorref.child('smokedetect').set('Clean');
+	}
+	
+}, the_interval);
