4f15c09c671013d0a4f111064930de1595c6ec05
DanRecher <recher.dan@gmail.com>
2016-10-18 16:22:51 +0300

    ssdfsdf

--- scripts/InitialSetup.js
+++ scripts/InitialSetup.js
@@ -6,7 +6,7 @@ var analogPin1 = new mraa.Aio(0); //to indecat if the door isclose or not useing
 var analogValue = analogPin1.read(); //read the value of the analog pin
 
 
-var minutes = 0.1, the_interval = minutes * 60 * 1000;
+var minutes = 0.01, the_interval = minutes * 60 * 1000;
 
 var doorconfig = require('./config'); // door configuration
 
