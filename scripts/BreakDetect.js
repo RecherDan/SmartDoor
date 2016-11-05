0388ae3517eb2b8567353fbabf4c7775f99609af
DanRecher <recher.dan@gmail.com>
2016-11-05 16:26:21 +0200

    sdfsdf

--- scripts/BreakDetect.js
+++ scripts/BreakDetect.js
@@ -13,7 +13,7 @@ var config = {
 	    storageBucket: "",
 	    messagingSenderId: "693048105512"
 	  };
-Firebase.initializeApp(config);
+Firebase.initializeApp(config, "break");
 
 var database = Firebase.database();
 var rootref = database.ref().child('doors');
